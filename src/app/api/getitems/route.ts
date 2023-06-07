a57d777138d9d2f17c9e1aada6f7286afa730998
K-Sikora <krzysieksikora00123@gmail.com>
2023-06-07 12:31:09 +0200

    Vercel fallback body test

--- src/app/api/getitems/route.ts
+++ src/app/api/getitems/route.ts
@@ -1,7 +1,43 @@
 import { NextResponse } from "next/server";
-
+type responseType = {
+  name: string;
+  weapon_type?: string;
+  type?: string;
+  rarity_color: string;
+  icon_url: string;
+  icon_url_large?: string;
+  price?: {
+    all_time?: {
+      average?: number;
+    };
+  };
+};
 export async function GET() {
   const response = await fetch("http://csgobackpack.net/api/GetItemsList/v2/");
   const data = await response.json();
-  return NextResponse.json({ data });
+  const items: responseType[] = data.items_list;
+  const itemsList = [];
+  for (let i in items) {
+    if (
+      (items[i].weapon_type?.toLowerCase() === "sniper rifle" ||
+        items[i].weapon_type?.toLowerCase() === "rifle" ||
+        items[i].weapon_type?.toLowerCase() === "pistol" ||
+        items[i].weapon_type?.toLowerCase() === "machinegun" ||
+        items[i].weapon_type?.toLowerCase() === "smg" ||
+        items[i].weapon_type?.toLowerCase() === "shotgun" ||
+        items[i].type?.toLowerCase() === "gloves") &&
+      !items[i].name.toLowerCase().includes("stattrak™")
+    ) {
+      itemsList.push({
+        name: items[i].name,
+        rarity_color: items[i].rarity_color,
+        icon_url: items[i].icon_url,
+        icon_url_large: items[i].icon_url_large,
+        price: items[i].price?.all_time?.average,
+      });
+    } else {
+      continue;
+    }
+  }
+  return NextResponse.json({ itemsList });
 }
