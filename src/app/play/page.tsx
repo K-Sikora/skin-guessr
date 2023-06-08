b18edf986f474015bd34e4689a616ee4b6bfd569
K-Sikora <krzysieksikora00123@gmail.com>
2023-06-08 12:41:10 +0200

    Removed deprecated package

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -5,7 +5,7 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import GameBoard from "@/components/GameBoard";
 import { RxSpeakerLoud, RxSpeakerOff } from "react-icons/rx";
-import Sound from "react-sound";
+
 const musicArray = [
   "https://cf-media.sndcdn.com/jzhOR38cB0JQ.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vanpoT1IzOGNCMEpRLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjg2MjIwMzEzfX19XX0_&Signature=O~n~N-K-Ymc-uvdH89mXcPxcmf92LqZSEKpdnZdiaXqjAdg8H4aFJR1ntmQCeUrqcHje5sZpFe-P3zUwAau2wve4Tj7uEIkk3KaTrSdtoXyul6Wt-pbOBdrLBMeOFIOxmESoeU6VCyNGtAC6JhI~ob9DTTuhhFQxpCcC5bgOPEF2WqiI-Lj1iUiXdOBN8Tud0SUELZsPm7-V8tWTM6b6FJdBz7GxOlmW~uwKmotD6bj5xn8IDoy3RoK4ZlaSFA52vMixNx55GVKtoDMVjQHDZGAIdaCMKamp9-1uTuaF9XLKalN7fsJs052zCSHEjeEp89HGQTcvwYZEkAKMpZAW7Q__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ",
   "https://cf-media.sndcdn.com/dL8J13exDWb9.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vZEw4SjEzZXhEV2I5LjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjg2MjIwNTcyfX19XX0_&Signature=FORRJ0zV4iQjkvYi1KWHxhxSNKPSMFQkcW5o1fO-ecrP3DqkzNQ3mYglnAL6781mEZ5IgfBcwanmglFx6x64Qvj9iWfcowzyPWHyjzaDACgRaoTnsoJW8XbtMSUwYbuf8mgRtqPIshUGgy0oXrN~jBaZB3XYsCX-3zB5~aZHW3hUBDgl9ZDAHPbRQ50eXte6QeloIsmBW6xSMYq1tZni272~leT4N4teTIj3S9hf6HLMAD87pTELCEgj2TQ5RCfk5ULdpHPpc9Q-jVAVzm626NhldmMkWbKwA9tI5CWTz~CrUYSlEf3d64a~91vrSwulNHbrfXAJYYkYfSDoEIDeSA__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ",
@@ -105,12 +105,6 @@ const PlayPage: React.FC = () => {
             ) : (
               <RxSpeakerOff className="text-white w-full h-full p-3" />
             )}
-            <Sound
-              url={musicArray[Math.floor(Math.random() * 3)]}
-              playStatus={
-                musicEnabled ? Sound.status.PLAYING : Sound.status.PAUSED
-              }
-            />
           </button>
         </div>
       )}
